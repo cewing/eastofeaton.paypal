2150bca264cd74425b3b8731f9b19d431478bd81
cewing <cris@crisewing.com>
2011-04-27 14:15:00 -0700

    initial package setup

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+version = '1.0a1'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='eastofeaton.paypal',
+      version=version,
+      description="A zca utility providing the paypal API for plone",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='paypal plone zope utility',
+      author='Cris Ewing',
+      author_email='cris@crisewing.com',
+      url='http://svn.plone.org/svn/collective/',
+      license='gpl',
+      packages=find_packages('src'),
+      namespace_packages=['eastofeaton'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'paypal',
+      ],
+      extras_require={'test': ['plone.app.testing']},
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
